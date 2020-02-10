b427cb5d1f32302c45119628c12b3caa8bc28481
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-02-10 00:31:23 -0500

    Update pymap.py

--- pymap.py
+++ pymap.py
@@ -1,7 +1,7 @@
 import pygame
 
 class tiled:
-    def __init__(self, map_name, map_size):
+    def __init__(self, map_name, map_size, map_textures_path):
         tilemap_template = {
             "name": None,
             "map_contents": [[]] #map_contents[layer_number][row][column]
@@ -9,6 +9,7 @@ class tiled:
         self.rows, self.columns = map_size
         self.tilemap = tilemap_template
         self.tilemap["name"] = map_name
+        self.map_textures_path = map_textures_path
         row_template = []
         for column in range(self.columns):
             row_template.append(0)
@@ -20,17 +21,29 @@ class tiled:
         self.tilemap["map_contents"][layer_id][row][column] = tile_id
     def get_name(self):
         return self.tilemap["name"]
+    def get_layers(self):
+        return len(self.tilemap["map_contents"])
     def get_rows(self):
         return len(self.tilemap["map_contents"][0])
     def get_columns(self):
         return len(self.tilemap["map_contents"][0][0])
     def get_tilemap(self):
         return self.tilemap
-    def save(self):
+    def save(self, file_path):
+        """Saves the map into a file"""
         pass
-    def render(self):
+    def pygame_render(self, location, surface, lighting=False):
         """Renders the whole map"""
-        pass
-    def render(self, layer_id):
+        for layer in range(self.get_layers()):
+            pygame_render(location, pygame_window, layer, lighting=lighting)
+    def pygame_render(self, location, surface, layer_id, lighting=False):
         """Renders a specific layer"""
-        pass
+        for row in range(self.get_rows):
+            for column in range(self.get_columns):
+                window.blit(
+                    tilemap.textures[tilemap.tilemap[row][column]], 
+                    (column*tilemap.tilesize, row*tilemap.tilesize)
+                )
+    def test_for_collusions(self, hitbox):
+        """Tests for collusions if the hitbox is inside a wall."""
+        x1,x2,y1,y2 = hitbox
