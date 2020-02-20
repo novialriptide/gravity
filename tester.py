354be065b767ce67c2ba7d2a333a276bb6768783
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-02-20 18:41:18 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -5,18 +5,24 @@ pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Tester")
 
-map_options = (
-    (3,4),
-    (5,5)
-)
-tile_map = pymap.tiled(map_options)
-tile_map.modify_tile((0,0),1)
+tileset = pymap.tileset("tiles.png", (5,5))
 
+tile_map = pymap.tiledmap((3,4),tileset)
+print(tile_map.get_tilemap())
+tile_map.modify_tile((0,0),1)
+print(tile_map.get_tilemap())
+tile_map.pygame_render_layer(0)
+
+print(tileset.get_tileset_size())
+print(tileset.get_tile_id_pos(17))
+
+"""
 running = True
 while(running):
-    screen.blit(tile_map.pygame_render((0,0), 0), (0,0))
+    screen.blit(tile_map.pygame_render_map((0,0), 0), (0,0))
 
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
               running = False
+"""
