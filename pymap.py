787ad9f8398d9ac9e475a0de632265943c473041
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-03-06 04:18:15 -0500

    Update pymap.py

--- pymap.py
+++ pymap.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class tileset:
     def __init__(self, textures, tile_size, tiles_distance=0):
         self.textures = pygame.image.load(textures)
@@ -20,9 +21,10 @@ class tileset:
         if tile_id != 0:
             tile_id = tile_id - 1
             tile = pygame.Surface(self.tile_size)
+            tile_pos = self.get_tile_id_pos(tile_id)
             tile.blit(self.textures, (0, 0), (
-                self.tile_size[0]*(self.get_tile_id_pos(tile_id))[0], 
-                self.tile_size[1]*(self.get_tile_id_pos(tile_id))[1],
+                self.tile_size[0]*tile_pos[0], 
+                self.tile_size[1]*tile_pos[1],
                 self.tile_size[0], 
                 self.tile_size[1]
             ))
@@ -42,38 +44,48 @@ class tiledmap:
         self.tile_size = tileset_class.tile_size
         self.map_size = (map_size[1], map_size[0])
         self.chunk_size = chunk_size
+        self.map_textures_path = tileset_class.textures
         self.tilemap = {
             #map_contents[layer_number][row][column]
-            "map_contents": [[[0 for j in range(self.map_size[0])] for i in range(self.map_size[1])]]
+            "map_contents": [[[0 for j in range(self.map_size[0])] for i in range(self.map_size[1])]],
+            "object_layer": 0
         }
-        self.map_textures_path = tileset_class.textures
-    def modify_tile(self, location, tile_id, layer_id=0):
+    def modify_layer(self, location, tile_id, layer_id=0):
         """Changes a tile ID texture"""
         row, column = location
-        self.tilemap["map_contents"][layer_id][row][column] = tile_id
+        if layer_id != self.tilemap["object_layer"]:
+            self.tilemap["map_contents"][layer_id][row][column] = tile_id
+        if layer_id == self.tilemap["object_layer"]:
+            self.tilemap["map_contents"][layer_id][row][column] = 1
     def get_tile_id(self, location, layer):
         row, column = location
         return self.tilemap["map_contents"][layer][row][column]
-    def get_layer_count(self):
-        return len(self.tilemap["map_contents"])
+    def add_new_layer(self):
+        self.tilemap["map_contents"].append([[0 for j in range(self.map_size[0])] for i in range(self.map_size[1])])
     def save(self, file_path, file_name):
         """Saves the map into a file"""
         pass
+    def pygame_get_sprites(self, render_size=1):
+        sprites_list = pygame.sprite.Group()
+        for row in range(self.map_size[0]+1):
+            for column in range(self.map_size[1]+1):
+                sprites_list.add(wall)
+        return sprites_list
     def pygame_render_chunk(self, chunk_location, render_size=1, lighting=False):
         """Renders a chunk of a layer of the map"""
         surface = pygame.Surface((
             self.tile_size[0]*self.chunk_size[0]*render_size,
-            self.tile_size[1].tile_size[1]()*self.chunk_size[1]*render_size
+            self.tile_size[1]*self.chunk_size[1]*render_size
         ))
     def pygame_render_layer(self, layer_id, render_size=1):
         """Renders a specific layer of the map"""
         surface = pygame.Surface((
-            self.tile_size[0]*(self.map_size)[0]*render_size,
-            self.tile_size[1]*(self.map_size)[1]*render_size
+            self.tile_size[0]*self.map_size[0]*render_size,
+            self.tile_size[1]*self.map_size[1]*render_size
         ))
         try:
-            for row in range((self.map_size)[0]+1):
-                for column in range((self.map_size)[1]+1):
+            for row in range(self.map_size[0]+1):
+                for column in range(self.map_size[1]+1):
                     tile_id = self.get_tile_id((row, column), layer_id)
                     surface.blit(
                         self.tileset.pygame_render(tile_id, render_size=render_size), 
@@ -86,15 +98,13 @@ class tiledmap:
     def pygame_render_map(self, render_size=1, lighting=False):
         """Renders the whole map"""
         surface = pygame.Surface((
-            self.tile_size[0]*(self.map_size)[0]*render_size,
-            self.tile_size[1]*(self.map_size)[1]*render_size
+            self.tile_size[0]*self.map_size[0]*render_size,
+            self.tile_size[1]*self.map_size[1]*render_size
         ))
-        for layer in range(self.get_layer_count()):
-            surface.blit(
-                pygame_render_layer(layer, render_size=1, lighting=lighting),
-                (0,0)
-            )
+        for layer in range(len(self.tilemap["map_contents"])):
+            if layer != self.tilemap["object_layer"]:
+                surface.blit(
+                    self.pygame_render_layer(layer, render_size=render_size),
+                    (0,0)
+                )
         return surface
-    def test_for_collusions(self, hitbox):
-        """Tests for collusions if the hitbox is inside a wall."""
-        x1,y1,x2,y2 = hitbox
