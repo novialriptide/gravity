8716594cc8f1fbcff2d45e1616c5bceb1293fd23
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-03-06 19:27:43 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,23 +1,26 @@
 import pygame
-import pymap
+import engine 
 
 pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Tester")
+render_size = 10
 
-tileset = pymap.tileset("tiles.png", (5,5))
+tileset = engine.tileset("tiles.png", (5,5), render_size=render_size)
 
-tile_map = pymap.tiledmap((3,4),tileset)
-tile_map.modify_layer((0,0),1)
+tile_map = engine.tiledmap((3,4),tileset)
+tile_map.modify_layer((0,0),1, layer_id=0)
 tile_map.add_new_layer()
 tile_map.modify_layer((0,0),1, layer_id=1)
+tile_map.set_render_size(render_size)
+print(tile_map.render_size)
 print(tile_map.tilemap)
-print(tile_map.pygame_get_sprites(render_size=10))
+print(tile_map.get_collision_rects())
 
 running = True
 while(running):
     screen.fill((100,100,100))
-    tilemap = tile_map.pygame_render_map(render_size=10)
+    tilemap = tile_map.pygame_render_map()
     screen.blit(tilemap, (0,0))
     pygame.display.flip()
     for event in pygame.event.get():
