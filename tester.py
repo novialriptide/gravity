a7847cdb92390a50d0d6615832e4b65c0dc096f4
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-03-12 18:38:56 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -8,23 +8,32 @@ render_size = 10
 
 tileset = engine.tileset("tiles.png", (5,5), render_size=render_size)
 
-tile_map = engine.tiledmap((3,4),tileset,(0,0))
-tile_map.modify_layer((0,0),1, layer_id=0)
+tile_map = engine.tiledmap((3,4),tileset,(0,0), render_size=render_size)
 tile_map.modify_layer((0,1),1, layer_id=0)
 tile_map.add_new_layer()
-tile_map.modify_layer((0,0),1, layer_id=1)
-tile_map.modify_layer((0,3),1, layer_id=1)
-tile_map.set_render_size(render_size)
+tile_map.modify_layer((0,1),1, layer_id=1)
+tile_map.generate_collision_rects()
 
-print(tile_map.tilemap)
-print(tile_map.get_collision_rects())
-print(tile_map.get_map_border_rect())
+player = engine.entity((0, 0), (5,5), map_class=tile_map, render_size=render_size)
+player_speed = 3
+player.force_texture_rect((220,20,220))
 
 running = True
 while(running):
     screen.fill((100,100,100))
     tile_map.pygame_render_map(screen, (0,0))
+    player.pygame_render(screen)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-              running = False
+            running = False
+        if event.type == pygame.KEYDOWN:
+            print(tile_map.collision_rects)
+            if event.key == pygame.K_w:
+                player.move((0,-player_speed), obey_collisions=True)
+            if event.key == pygame.K_s:
+                player.move((0,player_speed), obey_collisions=True)
+            if event.key == pygame.K_a:
+                player.move((-player_speed,0), obey_collisions=True)
+            if event.key == pygame.K_d:
+                player.move((player_speed,0), obey_collisions=True)
