123cc25e0702680633e99b2ae16fcdabce2e40a7
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-03-14 01:44:15 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -4,7 +4,7 @@ import engine
 pygame.init()
 screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Tester")
-render_size = 10
+render_size = 7
 
 tileset = engine.tileset("tiles.png", (5,5), render_size=render_size)
 
@@ -15,25 +15,35 @@ tile_map.modify_layer((0,1),1, layer_id=1)
 tile_map.generate_collision_rects()
 
 player = engine.entity((0, 0), (5,5), map_class=tile_map, render_size=render_size)
-player_speed = 3
+player_speed = 0.05
 player.force_texture_rect((220,20,220))
+player_movement = [0,0]
 
 running = True
 while(running):
     screen.fill((100,100,100))
     tile_map.pygame_render_map(screen, (0,0))
     player.pygame_render(screen)
+    player.move(player_movement, obey_collisions=True)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            print(tile_map.collision_rects)
             if event.key == pygame.K_w:
-                player.move((0,-player_speed), obey_collisions=True)
+                player_movement[1] = -player_speed
             if event.key == pygame.K_s:
-                player.move((0,player_speed), obey_collisions=True)
+                player_movement[1] = player_speed
             if event.key == pygame.K_a:
-                player.move((-player_speed,0), obey_collisions=True)
+                player_movement[0] = -player_speed
             if event.key == pygame.K_d:
-                player.move((player_speed,0), obey_collisions=True)
+                player_movement[0] = player_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                player_movement[1] = 0
+            if event.key == pygame.K_s:
+                player_movement[1] = 0
+            if event.key == pygame.K_a:
+                player_movement[0] = 0
+            if event.key == pygame.K_d:
+                player_movement[0] = 0
