2d286ecbdb3f8f0bf0960892ade69201d8928ad3
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-03-15 15:32:12 -0400

    Formatting changes

--- engine.py
+++ engine.py
@@ -1,15 +1,15 @@
 #######################################################
 #    GAME DEN ENGINE v1.0 (2020)                      #
-#    text_formating, tileset, tiledmap, entity        #
+#    text_formating,tileset,tiledmap,entity        #
 #######################################################
-# NOTE :: classes that have more than 8 methods,      #
+# NOTE :: classes that have more than 8 methods,     #
 #      :: they will be organized into categories      #
 #######################################################
 import pygame
 import os
 
 class text_formating:
-    def __init__(self, text, size, color, font="default", font_type="ttf", render_size=1):
+    def __init__(self,text,size,color,font="default",font_type="ttf",render_size=1):
         pygame.font.init()
         self.font = font
         self.size = size
@@ -19,67 +19,67 @@ class text_formating:
 
         if self.font == "default":
             # planned to implement custom font in v2.0 with GameDen's format
-            font_used = os.path.join("data", "Pixeled.ttf")
+            font_used = os.path.join("data","Pixeled.ttf")
             self.font_type = "ttf"
 
-        self.formatting = pygame.font.SysFont(font_used, size*render_size)
-        self.text_surface = self.formatting.render(text, False, color)
+        self.formatting = pygame.font.SysFont(font_used,size*render_size)
+        self.text_surface = self.formatting.render(text,False,color)
 
     def get_rect(self):
         """Returns a pygame Rect for collision"""
-        return pygame.Rect(self.position, (
+        return pygame.Rect(self.position,(
             self.formatting.get_width()*self.render_size,
             self.formatting.get_height()*self.render_size
         ))
     
-    def pygame_render(self, surface, position):
+    def pygame_render(self,surface,position):
         """Renders a text with the formatting applied"""
-        x, y = position
-        self.position = (x, y)
-        surface.blit(self.text_surface, (x, y))
+        x,y = position
+        self.position = (x,y)
+        surface.blit(self.text_surface,(x,y))
 
 class tileset:
-    def __init__(self, textures, tile_size, render_size=1, tiles_distance=0):
+    def __init__(self,textures,tile_size,render_size=1,tiles_distance=0):
         self.textures = pygame.image.load(textures)
         self.tile_size = tile_size
         self.tiles_distance = tiles_distance
         self.render_size = render_size
         self.tileset_size = (
-            int((self.textures.get_size())[0]/self.tile_size[0]), 
+            int((self.textures.get_size())[0]/self.tile_size[0]),
             int((self.textures.get_size())[1]/self.tile_size[1])
         )
 
-    def get_tile_id_pos(self, tile_id):
+    def get_tile_id_pos(self,tile_id):
         """Returns the position of the inputed tile ID"""
         if (self.tileset_size[0]*self.tileset_size[1]) > tile_id:
             return (
-                int(tile_id%(self.tileset_size)[0]), 
+                int(tile_id%(self.tileset_size)[0]),
                 int(tile_id/(self.tileset_size)[0])
             )
 
-    def set_render_size(self, render_size):
+    def set_render_size(self,render_size):
         """Changes the size of the tile when rendering"""
         self.render_size = render_size
 
-    def pygame_render(self, surface, position, tile_id):
+    def pygame_render(self,surface,position,tile_id):
         """Renders an image of a tile. tile_id can never be 0"""
         if tile_id != 0:
             tile_id = tile_id - 1
             tile = pygame.Surface(self.tile_size)
             tile_pos = self.get_tile_id_pos(tile_id)
-            tile.blit(self.textures, (0, 0), (
-                self.tile_size[0]*tile_pos[0], 
+            tile.blit(self.textures,(0,0),(
+                self.tile_size[0]*tile_pos[0],
                 self.tile_size[1]*tile_pos[1],
-                self.tile_size[0], self.tile_size[1]
+                self.tile_size[0],self.tile_size[1]
             ))
-            tile = pygame.transform.scale(tile, (
-                self.tile_size[0]*self.render_size, 
+            tile = pygame.transform.scale(tile,(
+                self.tile_size[0]*self.render_size,
                 self.tile_size[1]*self.render_size
             ))
-            surface.blit(tile, position)
+            surface.blit(tile,position)
 
 def generate_tiledmap(map_size):
-    width, height = map_size
+    width,height = map_size
     tilemap = {
         #map_contents[layer_number][row][column]
         "map_contents": [[[0 for j in range(width)] for i in range(height)]],
@@ -88,13 +88,13 @@ def generate_tiledmap(map_size):
     return tilemap
 
 class tiledmap:
-    def __init__(self, tiledmap, tileset_class, position, render_size=1, chunk_size=(5,5)):
+    def __init__(self,tiledmap,tileset_class,position,render_size=1,chunk_size=(5,5)):
         self.tileset = tileset_class
         self.tile_size = tileset_class.tile_size
         print(tiledmap)
         self.tilemap = tiledmap
         self.map_size = (
-            len(self.tilemap["map_contents"][0][0]), 
+            len(self.tilemap["map_contents"][0][0]),
             len(self.tilemap["map_contents"][0])
         )
         self.chunk_size = chunk_size
@@ -106,15 +106,15 @@ class tiledmap:
 
 ########################## OPTIONS METHODS #########################
 
-    def set_render_size(self, render_size):
+    def set_render_size(self,render_size):
         """Changes the size of the map when rendering"""
         self.render_size = render_size
 
 ########################### LAYER METHODS ##########################
 
-    def modify_layer(self, position, tile_id, layer_id=0):
+    def modify_layer(self,position,tile_id,layer_id=0):
         """Changes a tile ID texture"""
-        row, column = position
+        row,column = position
         if layer_id != self.tilemap["collision_layer"]:
             self.tilemap["map_contents"][layer_id][row][column] = tile_id
         if layer_id == self.tilemap["collision_layer"]:
@@ -126,9 +126,9 @@ class tiledmap:
 
 ############################ GET METHODS ###########################
 
-    def get_tile_id(self, position, layer):
+    def get_tile_id(self,position,layer):
         """Returns a tile ID from the specified position"""
-        row, column = position
+        row,column = position
         return self.tilemap["map_contents"][layer][row][column]
 
 ####################### GET/COLLISION METHODS ######################
@@ -139,13 +139,13 @@ class tiledmap:
         try:
             for row in range(self.map_size[0]+1):
                 for column in range(self.map_size[1]+1):
-                    tile_id = self.get_tile_id((row, column), self.tilemap["collision_layer"])
+                    tile_id = self.get_tile_id((row,column),self.tilemap["collision_layer"])
                     if tile_id == 1:
                         new_rect = pygame.Rect(((
-                            self.position[0]+column*self.tile_size[0]*self.render_size, 
+                            self.position[0]+column*self.tile_size[0]*self.render_size,
                             self.position[1]+row*self.tile_size[1]*self.render_size
                         ),(
-                            self.tile_size[0]*self.render_size, 
+                            self.tile_size[0]*self.render_size,
                             self.tile_size[1]*self.render_size
                         )))
                         collision_rects.append(new_rect)
@@ -160,13 +160,13 @@ class tiledmap:
     
     def get_map_border_rect(self):
         """Returns a pygame Rect for collision or camera from the specified position"""
-        return pygame.Rect(self.position, (
+        return pygame.Rect(self.position,(
             self.render_size*self.tile_size[0]*self.map_size[0],
             self.render_size*self.tile_size[1]*self.map_size[1]
         ))
 
-    def get_map_rect_collision(self, rect, movement):
-        vx, vy = movement
+    def get_map_rect_collision(self,rect,movement):
+        vx,vy = movement
         test_rect = rect.copy()
         test_rect.move((
             rect.x+vx,
@@ -180,11 +180,11 @@ class tiledmap:
         """Sets the map's position to a specific position"""
         self.position = self.original_position
 
-    def set_position(self, position):
+    def set_position(self,position):
         """Sets the map's position to a specific position"""
         self.position = position
 
-    def move(self, vposition):
+    def move(self,vposition):
         """Moves the map from its relative position"""
         self.position = (
             self.position[0]+vposition[0],
@@ -193,14 +193,14 @@ class tiledmap:
 
 ######################### RENDERING METHODS ########################
 
-    def pygame_render_chunk(self, surface, map_size, chunk_position, lighting=False):
+    def pygame_render_chunk(self,surface,map_size,chunk_position,lighting=False):
         """Renders a chunk of a layer of the map"""
         map_surface = pygame.Surface((
             self.tile_size[0]*self.chunk_size[0]*self.render_size,
             self.tile_size[1]*self.chunk_size[1]*self.render_size
         ))
 
-    def pygame_render_layer(self, surface, layer_id, lighting=False):
+    def pygame_render_layer(self,surface,layer_id,lighting=False):
         """Renders a specific layer of the map"""
         map_surface = pygame.Surface((
             self.tile_size[0]*self.map_size[0]*self.render_size,
@@ -209,39 +209,38 @@ class tiledmap:
         try:
             for row in range(self.map_size[0]+1):
                 for column in range(self.map_size[1]+1):
-                    tile_id = self.get_tile_id((row, column), layer_id)
-                    self.tileset.pygame_render(map_surface, (
-                        column*self.tile_size[0]*self.render_size, 
+                    tile_id = self.get_tile_id((row,column),layer_id)
+                    self.tileset.pygame_render(map_surface,(
+                        column*self.tile_size[0]*self.render_size,
                         row*self.tile_size[1]*self.render_size
-                    ), tile_id)
+                    ),tile_id)
         except IndexError:
-            surface.blit(map_surface, self.position)
+            surface.blit(map_surface,self.position)
 
-    def pygame_render_map(self, surface, lighting=False):
+    def pygame_render_map(self,surface,lighting=False):
         """Renders the whole map"""
         for layer in range(len(self.tilemap["map_contents"])):
             if layer != self.tilemap["collision_layer"]:
-                self.pygame_render_layer(surface, layer)
+                self.pygame_render_layer(surface,layer)
 
 class entity:
-    def __init__(self, position, size, tps=300, map_class=None, render_size=1):
-        x, y = position
-        width, height = size
-        self.position = (x, y)
-        self.size = (width, height)
+    def __init__(self,position,size,tps=300,map_class=None,render_size=1):
+        x,y = position
+        width,height = size
+        self.position = (x,y)
+        self.size = (width,height)
         self.entity_data = {"animation_sprites": {}}
         self.render_size = render_size
         self.tick = 0
         self.tps = tps
         self.map_class = map_class
         self.current_texture = None
-        self.animation_status = False
 
-        self.original_position = (position[0], position[1])
+        self.original_position = (position[0],position[1])
 
 ########################## OPTIONS METHODS #########################
 
-    def set_render_size(self, render_size):
+    def set_render_size(self,render_size):
         """Changes the size of the entity when rendering"""
         self.render_size = render_size
 
@@ -249,7 +248,7 @@ class entity:
 
     def get_rect(self):
         """Returns a pygame Rect for collision"""
-        return pygame.Rect(self.position, (
+        return pygame.Rect(self.position,(
             self.size[0]*self.render_size,
             self.size[1]*self.render_size
         ))
@@ -260,59 +259,59 @@ class entity:
         """Sets the entity's position to a specific position"""
         self.position = self.original_position
 
-    def set_position(self, position):
+    def set_position(self,position):
         """Sets the entity's position to a specific position"""
         self.position = position
 
-    def move(self, vposition, obey_collisions=False):
+    def move(self,vposition,obey_collisions=False):
         """Moves the entity from its relative position"""
-        vx, vy = vposition
+        vx,vy = vposition
         if obey_collisions == False:
-            self.position = (self.position[0]+vx, self.position[1]+vy)
+            self.position = (self.position[0]+vx,self.position[1]+vy)
         if obey_collisions == True:
-            if self.map_class.get_map_rect_collision(self.get_rect(), (vx, vy)):
-                self.position = (self.position[0]+vx, self.position[1]+vy)
-            if self.map_class.get_map_rect_collision(self.get_rect(), (vx, vy)) == False:
-                self.position = (self.position[0]-vx, self.position[1]-vy)
+            if self.map_class.get_map_rect_collision(self.get_rect(),(vx,vy)):
+                self.position = (self.position[0]+vx,self.position[1]+vy)
+            if self.map_class.get_map_rect_collision(self.get_rect(),(vx,vy)) == False:
+                self.position = (self.position[0]-vx,self.position[1]-vy)
 
 ######################## PLACEHOLDER METHODS #######################
 
-    def texture_color_rect(self, color):
+    def texture_color_rect(self,color):
         """Returns a Rect of the entity in 1 color"""
         texture = pygame.Surface((
             self.size[0]*self.render_size,
             self.size[1]*self.render_size
         ))
-        pygame.draw.rect(texture, color, (
-            0, 0, 
-            self.size[0]*self.render_size, 
+        pygame.draw.rect(texture,color,(
+            0,0,
+            self.size[0]*self.render_size,
             self.size[1]*self.render_size
         ))
         return texture
 
-    def force_texture_rect(self, color):
+    def force_texture_rect(self,color):
         """Forces the texture of the entity to be a pygame Rect"""
         texture = pygame.Surface((
             self.size[0]*self.render_size,
             self.size[1]*self.render_size
         ))
-        pygame.draw.rect(texture, color, (
-            self.position[0], self.position[1], 
-            self.size[0]*self.render_size, 
+        pygame.draw.rect(texture,color,(
+            self.position[0],self.position[1],
+            self.size[0]*self.render_size,
             self.size[1]*self.render_size
         ))
         self.current_texture = texture
 
 ######################### ANIMATION METHODS ########################
 
-    def set_tps(self, tps):
+    def set_tps(self,tps):
         self.tps = tps
 
     def update_animation_tick(self):
         """Updates animation tick"""
         self.tick += 1
 
-    def play_animation(self, animation_dict_name): # UNTESTED
+    def play_animation(self,animation_dict_name): # UNTESTED
         """Starts the animation"""
         number_of_sprites = len(self.entity_data["animation_sprites"][animation_dict_name])
         if self.tick == number_of_sprites*self.tps:
@@ -322,13 +321,17 @@ class entity:
     def stop_animation(self):
         """Stops the animation"""
         self.tick = 0
+
+    def reset_animation(self):
+        """Stops the animation and resets it to it's original sprite"""
+        self.tick = 0
     
-    def new_animation_data(self, name, data):
+    def new_animation_data(self,name,data):
         """Stores the entity's sprite information. Inside the data variable should be a list of the sprite images in order"""
         self.entity_data["animation_sprites"][str(name)] = data
 
 ######################### RENDERING METHODS ########################
 
-    def pygame_render(self, surface):
+    def pygame_render(self,surface):
         """Renders the entity's sprite"""
-        surface.blit(self.current_texture, self.position)
+        surface.blit(self.current_texture,self.position)
--- tester.py
+++ tester.py
@@ -8,52 +8,52 @@ screen = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Tester")
 render_size = 7
 
-tileset = engine.tileset(os.path.join("data", "tiles.png"), (5,5), render_size=render_size)
+tileset = engine.tileset(os.path.join("data","tiles.png"),(5,5),render_size=render_size)
 
 tiled_map_contents = engine.generate_tiledmap((4,3))
 
-tile_map = engine.tiledmap(tiled_map_contents,tileset,(0,0), render_size=render_size)
-tile_map.modify_layer((0,1),1, layer_id=0)
+tile_map = engine.tiledmap(tiled_map_contents,tileset,(0,0),render_size=render_size)
+tile_map.modify_layer((0,1),1,layer_id=0)
 tile_map.add_new_layer()
-tile_map.modify_layer((0,1),1, layer_id=1)
+tile_map.modify_layer((0,1),1,layer_id=1)
 tile_map.generate_collision_rects()
 
-player = engine.entity((0, 0), (5,5), map_class=tile_map, render_size=render_size)
+player = engine.entity((0,0),(5,5),map_class=tile_map,render_size=render_size)
 player_speed = 0.05
-player.force_texture_rect((220,20,220))
 player_movement = [0,0]
 
-player.new_animation_data("forward", [
+player.new_animation_data("idle",[
+    player.texture_color_rect((231,0,127))
+])
+
+player.new_animation_data("forward",[
     player.texture_color_rect((231,124,127)),
     player.texture_color_rect((0,20,127)),
     player.texture_color_rect((101,30,167)),
 ])
-player.new_animation_data("backward", [
+player.new_animation_data("backward",[
     player.texture_color_rect((231,104,127)),
     player.texture_color_rect((0,20,0)),
     player.texture_color_rect((101,130,0)),
 ])
-player.new_animation_data("left", [
+player.new_animation_data("left",[
     player.texture_color_rect((231,124,0)),
     player.texture_color_rect((0,20,127)),
     player.texture_color_rect((0,130,167)),
 ])
-player.new_animation_data("right", [
+player.new_animation_data("right",[
     player.texture_color_rect((0,0,127)),
     player.texture_color_rect((0,20,0)),
     player.texture_color_rect((101,130,167)),
 ])
 
-text1 = engine.text_formating("If everything seems to be working, everything is working.", 20, (255,255,255))
+text1 = engine.text_formating("If everything seems to be working,everything is working.",20,(255,255,255))
 
 running = True
 while(running):
-    screen.fill((100,100,100))
-    tile_map.pygame_render_map(screen, (0,0))
-    player.pygame_render(screen)
-    player.move(player_movement, obey_collisions=True)
-    text1.pygame_render(screen, (0,300))
-
+    if player_movement == [0,0]:
+        player.play_animation("idle")
+        player.update_animation_tick()
     if player_movement[1] > 0:
         player.play_animation("backward")
         player.update_animation_tick()
@@ -66,6 +66,12 @@ while(running):
     if player_movement[0] > 0:
         player.play_animation("right")
         player.update_animation_tick()
+
+    screen.fill((100,100,100))
+    tile_map.pygame_render_map(screen,(0,0))
+    player.pygame_render(screen)
+    player.move(player_movement,obey_collisions=True)
+    text1.pygame_render(screen,(0,300))
 
     pygame.display.flip()
     for event in pygame.event.get():
