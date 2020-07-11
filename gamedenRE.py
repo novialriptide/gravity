67b062662a8ca92bb683f617856a867d7897d780
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2020-07-11 03:05:53 -0400

    r

--- gamedenRE.py
+++ gamedenRE.py
@@ -1,7 +1,69 @@
 import pygame
+import os
 import json
+import numpy
+import math
 
-def convert_tiledjson(path, collision_layer: int = None, invisible_layers: list = []):
+def _calculate_segment_intersection(x1,y1,x2,y2,x3,y3,x4,y4):
+    exception_msg = "two lines inputted are parallel or coincident"
+
+    dem = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+    if dem == 0:
+        raise Exception(exception_msg)
+
+    t1 = (x1-x3)*(y3-y4) - (y1-y3)*(x3-x4)
+    t = t1/dem
+    
+    u1 = (x1-x2)*(y1-y3) - (y1-y2)*(x1-x3)
+    u = -(u1/dem)
+
+    if t >= 0 and t <= 1 and u >= 0 and u <= 1:
+        Px = x1 + t*(x2-x1)
+        Py = y1 + t*(y2-y1)
+        return Px, Py
+    else:
+        raise Exception(exception_msg)
+
+def convert_rect_to_wall(rect):
+    return (rect.left, rect.top, rect.right, rect.top), (rect.left, rect.bottom, rect.right, rect.bottom), (rect.left, rect.top, rect.left, rect.bottom), (rect.right, rect.top, rect.right, rect.bottom)
+
+def convert_rects_to_walls(rects):
+    walls = []
+    for rect in rects:
+        wall_lines = convert_rect_to_wall(rect)
+        for wall_line in range(len(wall_lines)):
+            walls.append(wall_lines[wall_line])
+    return walls
+
+def get_ray_endpoint(coord1,coord2,walls):
+    x1, y1 = coord1
+    x2, y2 = coord2
+    line_length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    highest_point = (x2, y2)
+    highest_point_length = line_length
+    for wall in walls:
+        try:
+            c = _calculate_segment_intersection(x1, y1, x2, y2, wall[0], wall[1], wall[2], wall[3])
+            c_length = math.sqrt((x1-c[0])**2 + (y1-c[1])**2)
+            if highest_point_length > c_length:
+                highest_point = c
+                highest_point_length = c_length
+        except: pass
+    return highest_point
+
+def get_v_movement(degree,speed):
+    radian = math.radians(degree) 
+    x_distance = math.cos(radian)*speed
+    y_distance = math.sin(radian)*speed
+    return [x_distance, y_distance]
+
+def text(text,size,font,color,render_size=1):
+    pygame.font.init()
+    formatting = pygame.font.SysFont(font,size*render_size)
+    text_surface = formatting.render(text,True,color)
+    return text_surface
+
+def convert_tiledjson(path):
     """Converts a tiled json map in GameDen's formatting"""
     with open(path, 'r') as file:
         loaded_json = json.load(file)
@@ -15,13 +77,51 @@ def convert_tiledjson(path, collision_layer: int = None, invisible_layers: list 
     tilemap = {
         #contents[layer_number][row][column]
         "contents": contents,
-        "collision_layer": collision_layer,
-        "invisible_layers": invisible_layers
+        "collision_layer": None,
+        "invisible_layers": []
     }
     return tilemap
 
+def grayscale(img):
+    """Heavy function, turns an image to black and white"""
+    arr = pygame.surfarray.array3d(img)
+
+    avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
+    arr = numpy.array([[[avg,avg,avg] for avg in col] for col in avgs])
+    return pygame.surfarray.make_surface(arr)
+
+class button:
+    def __init__(self, position: tuple, image_path, render_size: int = 1):
+        x, y = position
+        self.position = [x,y]
+        self.image = pygame.image.load(image_path)
+        self.render_size = render_size
+
+    @property
+    def rect(self):
+        x, y = self.position
+        return pygame.Rect(x, y,
+            self.image.get_width()*self.render_size, 
+            self.image.get_height()*self.render_size
+        )
+
+    def is_hovering(self, mouse_position: tuple) -> bool:
+        """If the position inputed is on top of the button, it'll return True"""
+        return self.rect.collidepoint(mouse_position)
+
+    def pygame_render(self,surface):    
+        image = pygame.transform.scale(self.image,(
+            self.image.get_width()*self.render_size, 
+            self.image.get_height()*self.render_size
+        ))
+        surface.blit(image, (self.rect.x, self.rect.y))
+
 class tileset:
-    def __init__(self,textures_path,tile_size,tiles_distance=0):
+    def __init__(self, 
+        textures_path: str, 
+        tile_size: tuple, 
+        tiles_distance: int = 0
+    ):
         self.textures = pygame.image.load(textures_path)
         self.tile_size = tile_size
         self.tiles_distance = tiles_distance
@@ -96,10 +196,10 @@ class tilemap:
         except TypeError:
             raise Exception(f"tile location doesn't exist ({row}, {column})")
 
-    def get_collision_rects(self, layer: int, render_size: int = 1) -> list:
+    def get_collision_rects(self, position: tuple, layer: int, render_size: int = 1) -> list:
         """Returns a list of pygame Rects from a layer from the specified position"""
         collision_rects = []
-        x, y == self.position
+        x, y = position
         t_width, t_height = self.tile_size
         m_width, m_height = self.map_size
 
@@ -124,7 +224,7 @@ class tilemap:
     def create_new_layer(self):
         self.tilemap["contents"].append([[0 for j in range(self.map_size[0])] for i in range(self.map_size[1])])
     
-    def pygame_render_layer(self, position, surface, layer_id: int, render_size: int = 1):
+    def get_image_layer(self, layer_id: int, render_size: int = 1):
         t_width, t_height = self.tile_size
         m_width, m_height = self.map_size
         map_surface = pygame.Surface((
@@ -141,9 +241,142 @@ class tilemap:
                     row*t_height*render_size
                 ), map_surface, tile_id, render_size=render_size)
 
-        surface.blit(map_surface,position)
+        return map_surface
 
-    def pygame_render_map(self, position, surface, render_size: int = 1):
+    def get_image_map(self, render_size: int = 1):
+        t_width, t_height = self.tile_size
+        m_width, m_height = self.map_size
+        map_surface = pygame.Surface((
+            t_width*m_width*render_size,
+            t_height*m_height*render_size
+        ), pygame.SRCALPHA, 32)
+        map_surface = map_surface.convert_alpha()
+
         for layer in range(len(self.map_data["contents"])):
             if layer not in self.map_data["invisible_layers"]:
-                self.pygame_render_layer(position,surface,layer,render_size=render_size)
+                map_surface.blit(self.get_image_layer(layer,render_size=render_size), (0,0))
+        
+        return map_surface
+
+class entity:
+    def __init__(self, rect, tps, tilemap=None):
+        self.rect = rect
+        self.tps = tps
+        self.tilemap = tilemap
+
+        # animations
+        self.tick = 0
+        self.current_texture = None
+        self.image_offset_position = [0,0]
+
+        # physics
+        self.vertical_momentum = 0
+        self.is_floating_tick = 0
+        self.weight = 3
+        self.jump_speed = 0.2
+
+        # misc
+        self.position_float = [float(self.rect.x), float(self.rect.y)]
+
+    def collision_test(self, rect, tiles: list) -> list:
+        hit_list = []
+        for tile in tiles:
+            if rect.colliderect(tile):
+                hit_list.append(tile)
+        return hit_list
+    
+    def set_position(self,position: tuple):
+        x, y = position
+        self.rect.x = x
+        self.rect.y = y
+        self.position_float = position
+    
+    def set_position2(self, position: tuple, tilemap, tilemap_render_size: int = 1):
+        x, y = position
+        m_x, m_y = tilemap.position
+        t_width, t_height = tilemap.tile_size
+
+        self.rect.x = m_x+t_width*tilemap.render_size*x
+        self.rect.y = m_y+t_height*tilemap.render_size*y
+        self.position_float = [self.rect.x, self.rect.y]
+    
+    def jump(self, jump_height, fall_speed):
+        if self.is_floating_tick < jump_height:
+            self.vertical_momentum = -fall_speed
+
+    def move(self, 
+        movement: tuple, 
+        collisions: list, 
+        direction=["bottom", "top"],
+        movement_accurate: bool = False
+    ) -> dict:
+        valid_gravity_settings = [
+            ["bottom", "top"],
+            ["top", "bottom"],
+            ["left", "right"],
+            ["right", "left"]
+        ]
+        collision_types = {
+            "top": False,
+            "right": False,
+            "bottom": False,
+            "left": False
+        }
+        valid_directions = collision_types.keys()
+
+        # physics: deals with the entity if it's in the air
+        if direction[0] in valid_directions and direction[1] in valid_directions:
+            if direction == valid_gravity_settings[0]: movement[1] += self.vertical_momentum
+            if direction == valid_gravity_settings[1]: movement[1] -= self.vertical_momentum
+            if direction == valid_gravity_settings[2]: movement[0] -= self.vertical_momentum
+            if direction == valid_gravity_settings[3]: movement[0] += self.vertical_momentum
+            self.vertical_momentum += self.jump_speed
+            if self.vertical_momentum > self.weight:
+                self.vertical_momentum = self.weight
+        elif direction[0] == None and direction[1] == None: pass
+        else: raise Exception(f"invalid direction(s) \"{direction}\"")
+
+        if movement_accurate:
+            self.position_float[0] += movement[0]
+            self.rect.x += movement[0] + (self.position_float[0] - self.rect.x)
+        else: self.rect.x += movement[0]
+
+        hit_list = self.collision_test(self.rect,collisions)
+        for tile in hit_list:
+            if movement[0] > 0:
+                self.rect.right = tile.left
+                collision_types["right"] = True
+            elif movement[0] < 0:
+                self.rect.left = tile.right
+                collision_types["left"] = True
+
+        if movement_accurate:
+            self.position_float[1] += movement[1]
+            self.rect.y += movement[1] + (self.position_float[1] - self.rect.y)
+        else: self.rect.y += movement[1]
+
+        hit_list = self.collision_test(self.rect,collisions)
+        for tile in hit_list:
+            if movement[1] > 0:
+                self.rect.bottom = tile.top
+                collision_types["bottom"] = True
+            elif movement[1] < 0:
+                self.rect.top = tile.bottom
+                collision_types["top"] = True
+        
+        # physics: returns the state to normal
+        if collision_types[direction[0]] == True:
+            self.is_floating_tick = 0
+            self.vertical_momentum = 0
+        else: self.is_floating_tick += 1
+        if collision_types[direction[1]] == True:
+            self.is_floating_tick = 0
+            self.vertical_momentum = 0
+
+        return collision_types
+
+class physics:
+    def __init__(self): pass
+    def calculate_projectile_motion(self, position: tuple, init_speed: float, tick: float):
+        x, y = position
+        return -16*(tick^2)+init_speed*tick+y
--- tester.py
+++ tester.py
@@ -9,19 +9,79 @@ import json
 import gamedenRE
 
 SCREEN_SIZE = (500,500)
+RENDER_SIZE = 1/10
 
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("GameDenREWRITE Tester")
 
+# default tile map setup
 default_tileset = gamedenRE.tileset("test.png",(500,500))
 test_tilemap = gamedenRE.convert_tiledjson("test.json")
 d_tilemap = gamedenRE.tilemap(test_tilemap, default_tileset)
-print(test_tilemap)
+d_tilemap_image = d_tilemap.get_image_map(render_size=RENDER_SIZE)
+map_pos = (0,0)
+
+# player entity setup
+t_width, t_height = d_tilemap.tile_size
+start_pos = (t_width*2*RENDER_SIZE, t_height*1*RENDER_SIZE)
+player_rect = pygame.Rect(start_pos[0], start_pos[1], 20, 20)
+player = gamedenRE.entity(player_rect, 300, d_tilemap)
+player.weight = 5
+player.jump_speed = 0.2
+player_speed = 5
+
+moving_right = False
+moving_left = False
+
+valid_gravity_settings = [
+    ["bottom", "top"],
+    ["top", "bottom"],
+    ["left", "right"],
+    ["right", "left"]
+]
+current_gravity_setting = valid_gravity_settings[0]
+
 while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
               sys.exit()
+        if event.type == pygame.KEYDOWN:
+            """
+            if event.key == pygame.K_RIGHT:
+                moving_right = True
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            if event.key == pygame.K_UP:
+                if player.is_floating_tick < 6:
+                    player.vertical_momentum = -10
+            """
+            if event.key == pygame.K_s: current_gravity_setting = valid_gravity_settings[0]
+            if event.key == pygame.K_w: current_gravity_setting = valid_gravity_settings[1]
+            if event.key == pygame.K_a: current_gravity_setting = valid_gravity_settings[2]
+            if event.key == pygame.K_d: current_gravity_setting = valid_gravity_settings[3]
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                moving_right = False
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+
+    player_movement = [0,0]
+    if moving_right == True:
+        player_movement[0] += player_speed
+    if moving_left == True:
+        player_movement[0] -= player_speed
+    collision_rects = d_tilemap.get_collision_rects(map_pos, 0, render_size=RENDER_SIZE)
+
     screen.fill((115, 115, 115))
-    d_tilemap.pygame_render_map((0,0),screen,render_size=1/10)
+    screen.blit(d_tilemap_image, map_pos)
+    player.move(player_movement, collision_rects, direction=current_gravity_setting)
+    pygame.draw.rect(screen, (100,100,100), player.rect)
+
+    w_width, w_height = SCREEN_SIZE
+    fps_text = gamedenRE.text(f"FPS: {int(clock.get_fps())}", 15, "Arial", (0,0,0))
+    screen.blit(fps_text, (int(w_width - w_width/75 - fps_text.get_rect().width),int(w_height/75)))
+
     pygame.display.flip()
+    clock.tick(60)
