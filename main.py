7ce3271f8046e847336f28f5209094d8d7322bd1
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2020-07-29 02:52:23 -0400

    retry button added

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ GOAL_TILE_ID = 3
 START_TILE_ID = 4
 
 # vars
+attempt_number = 0
 game_over = False
 game_won = False
 
@@ -55,11 +56,26 @@ back_objects = []
 space = pymunk.Space()
 
 # tile map setup
+player_pos = 0,0
 map_pos = [0,0]
 loaded_tileset = None
 loaded_tilemap = None
 loaded_tilemap_image = None
 level_number = 1
+
+def execute_data_points(tilemap: gamedenRE.tilemap, layer: int):
+    """currently just marks the spawn point"""
+    global player_pos
+    m_width, m_height = tilemap.map_size
+    t_width, t_height = tilemap.tile_size
+    for row in range(m_height):
+        for column in range(m_width):
+            tile_id = tilemap.map_data["contents"][layer][row][column]
+
+            # sets the player's spawn position
+            if tile_id == 1:
+                player_pos =  t_width*column*RENDER_SIZE, t_height*row*RENDER_SIZE
+
 def load_tilemap(tileset: gamedenRE.tileset, tilemap_path: str):
     global loaded_tileset
     global loaded_tilemap
@@ -67,17 +83,19 @@ def load_tilemap(tileset: gamedenRE.tileset, tilemap_path: str):
 
     loaded_tileset = tileset
     loaded_tilemap = gamedenRE.convert_tiledjson(tilemap_path)
+    loaded_tilemap["invisible_layers"] = [0]
     loaded_tilemap = gamedenRE.tilemap(loaded_tilemap, loaded_tileset)
     loaded_tilemap_image = loaded_tilemap.get_image_map()
     t_w, t_h = loaded_tilemap_image.get_rect().width, loaded_tilemap_image.get_rect().height
     loaded_tilemap_image = pygame.transform.scale(loaded_tilemap_image, (int(t_w*RENDER_SIZE), int(t_h*RENDER_SIZE)))
+    execute_data_points(loaded_tilemap, 0)
 
 load_tilemap(gamedenRE.tileset("textures/tilesets/1.png", (500,500)), "levels/LEVEL_1.json")
 
 # player entity setup
 t_width, t_height = loaded_tilemap.tile_size
 body = pymunk.Body(100, 1666)
-body.position = t_width*2*RENDER_SIZE, t_height*3*RENDER_SIZE
+body.position = player_pos
 player = gamedenRE.entity(body, [200*RENDER_SIZE,200*RENDER_SIZE])
 player.poly.friction = 1
 player.poly.elasticity = 0
@@ -125,7 +143,14 @@ collision_handler.begin = coll_begin
 collision_handler.pre_solve = coll_pre
 collision_handler.post_solve = coll_post
 collision_handler.separate = coll_separate
-polys = gamedenRE.rects_to_polys(space, loaded_tilemap.get_collision_rects((0,0), 0, render_size=RENDER_SIZE))
+polys = gamedenRE.rects_to_polys(space, loaded_tilemap.get_collision_rects((0,0), 1, render_size=RENDER_SIZE))
+
+# game functions
+def game_reset():
+    execute_data_points(loaded_tilemap, 0)
+    space.gravity = 0,0
+    player.body.position = player_pos
+    player.body.velocity = 0,0
 
 # title screen
 display_title_screen = True
@@ -156,6 +181,9 @@ while(True):
     if clock.get_fps() != 0: dt = clock.get_fps()/1000
     else: dt = 0
 
+    left_mouse_click = False
+    right_mouse_click = False
+    mouse_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -164,6 +192,9 @@ while(True):
             if event.key == pygame.K_w: space.gravity = 0,dt*-gravity_speed
             if event.key == pygame.K_a: space.gravity = dt*-gravity_speed,0
             if event.key == pygame.K_d: space.gravity = dt*gravity_speed,0
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                left_mouse_click = True
 
     if game_over:
         # background
@@ -171,8 +202,19 @@ while(True):
         
         # HUD
         w_width, w_height = SCREEN_SIZE
-        text = gamedenRE.text(f"You lost!", 15, "Arial", (0,0,0))
-        screen.blit(text, (int(w_width/2 - text.get_rect().width/2),int(w_height/2 - text.get_rect().height/2)))
+        text = gamedenRE.text(f"Click on me to retry.", 15, "Arial", (0,0,0))
+
+        # button
+        button_pos = [int(w_width/2 - text.get_rect().width/2),int(w_height/2 - text.get_rect().height/2)]
+        button_rect = pygame.Rect(button_pos[0], button_pos[1], text.get_rect().width, text.get_rect().height)
+        button = gamedenRE.button(button_rect)
+        pygame.draw.rect(screen, (0,255,0), button_rect)
+        if left_mouse_click and button.is_hovering(mouse_pos):
+            game_reset()
+            game_over = False
+            attempt_number += 1
+
+        screen.blit(text, button_pos)
     
     elif game_won:
         # background
@@ -219,8 +261,8 @@ while(True):
     fps_text = gamedenRE.text(f"FPS: {int(clock.get_fps())}", 15, "Arial", (0,0,0))
     screen.blit(fps_text, (int(w_width - w_width/75 - fps_text.get_rect().width),int(w_height/75)))
 
-    level_text = gamedenRE.text(f"level loaded: {level_number}", 15, "Arial", (0,0,0))
-    screen.blit(level_text, (int(w_width - w_width/75 - level_text.get_rect().width),fps_text.get_rect().height+int(w_height/75)))
+    attempt_text = gamedenRE.text(f"attempt #{attempt_number}", 15, "Arial", (0,0,0))
+    screen.blit(attempt_text, (int(w_width - w_width/75 - attempt_text.get_rect().width),fps_text.get_rect().height+int(w_height/75)))
 
     pygame.display.flip()
     space.step(0.02)
