d5391d9585283f300f30b06e4958b38e5909e8e2
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2020-08-04 03:06:01 -0400

    Update messaging.py

--- messaging.py
+++ messaging.py
@@ -1,11 +1,35 @@
+class user:
+    def __init__(self, displayname: str):
+        self.displayname = displayname
+
+class message:
+    def __init__(self, user: user, content: str):
+        self.user = user
+        self.content = content
+        self.id = None
+
 class channel:
     def __init__(self):
         self.users = []
         self.messages = []
+    
+    def log(self, message: message):
+        message.id = len(self.messages)
+        self.messages.append(message)
+        return message
+    
+    def log_raw(self, content: str):
+        message = message(None, content)
+        self.messages.append(message)
+        return message
 
-class message:
-    def __init__(self, user, content):
-        pass
+'''
+novial = user("novial")
+general_chat = channel()
 
-class user:
-    pass
+msg = message(novial, "Hello World")
+general_chat.log(msg)
+
+for message in general_chat.messages:
+    print(f"({message.user.displayname}) {message.content}")
+'''
